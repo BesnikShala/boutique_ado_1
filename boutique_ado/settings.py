fcda5d953d6279848ebe825c90115958970d366a
BesnikShala <bes.shala@gmail.com>
2022-01-10 14:39:28 +0000

    make it better

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -118,16 +118,18 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-DATABASES = {
-    'default': dj_database_url.parse('postgres://bnbjbymzgvijtw:96b83cbbaa1765d4acff4372406df9191f8633d6be504b635c60380491035680@ec2-52-211-158-144.eu-west-1.compute.amazonaws.com:5432/d77l8ks13sqev7')
-}
 
 
 # Password validation
