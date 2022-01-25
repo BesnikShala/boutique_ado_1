b1fdfd48a52d74deba03270fdab3809cb0ab3da3
BesnikShala <bes.shala@gmail.com>
2022-01-25 22:00:39 +0000

    Try to fix issues after gitpod crash

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'sk_test_51KDxcSLzZ15CXB74iKGhx0SB2Ea4fem3IKvBOPXoaSZ5sKq6wD5DMLZXq9IsIyXEJETq5GRhD80fNfofh5Lp4tUg00DyBHlg1u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
